73d7b2eea50e6baceb4c197ef751b7b987f4c2d5
bmkim <bomingeem@gmail.com>
2020-12-01 14:10:16 +0900

    1차원 배열

--- src/study/Main.java
+++ src/study/Main.java
@@ -5,25 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[26];
-        Scanner scanner = new Scanner(System.in);
 
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = -1;
-        }
-
-        String word = scanner.nextLine();
-
-        for (int i=0; i<word.length(); i++) {
-            char ch = word.charAt(i);
-
-            if(arr[ch - 'a'] == -1) {
-                arr[ch - 'a'] = i;
-            }
-        }
-
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
     }
 }
