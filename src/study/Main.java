7ac19bd6f43f2992daf96c41f2728d6f6c723817
bmkim <bomingeem@gmail.com>
2020-12-17 15:31:22 +0900

    소수

--- src/study/Main.java
+++ src/study/Main.java
@@ -5,27 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        boolean[] check = new boolean[b+1];
 
-        //true = 소수아님, false = 소수
-        check[0] = check[1] = true;
-
-        for(int i=2; i*i<=b; i++) {
-            if (check[i] == true) { //소수의 배수만 체크
-                continue;
-            }
-
-            for(int j=i+i; j<=b; j+=i) {
-                check[j] = true;
-            }
-        }
-        for (int i=a; i<=b; i++) {
-            if (check[i] == false) {
-                System.out.println(i);
-            }
-        }
     }
 }
