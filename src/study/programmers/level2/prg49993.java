36aa65988658643203ee6023a5f7d779348d4752
bomingeem <bomingeem@gmail.com>
2021-04-16 17:35:26 +0900

    [feat] 스킬트리: inner class로 변경

--- src/study/programmers/level2/prg49993.java
+++ src/study/programmers/level2/prg49993.java
@@ -3,29 +3,38 @@ package study.programmers.level2;
 public class prg49993 {
     public static void main(String[] args) {
         //스킬트리
-        int answer = 0;
         String skill = "CBD";
         String[] skill_trees = {"BACDE", "CBADF", "AECB", "BDA"};
+        Solution solution = new Solution();
 
-        int index = 0;
-        while (true) {
-            String skill_tree = skill_trees[index];
-            String skill_tree_temp = skill_tree;
-            int size = skill_tree.length();
+        System.out.println(solution.solution(skill, skill_trees));
+    }
 
-            for(int i=0; i<size; i++) {
-                String charSkill = String.valueOf(skill_tree_temp.charAt(i));
-                if (!skill.contains(charSkill)) {
-                    skill_tree = skill_tree.replace(charSkill, "");
+    static class Solution {
+        public int solution(String skill, String[] skill_trees) {
+            int answer = 0;
+
+            int index = 0;
+            while (true) {
+                String skill_tree = skill_trees[index];
+                String skill_tree_clone = new String(skill_tree);
+                int size = skill_tree.length();
+
+                for(int i=0; i<size; i++) {
+                    String charSkill = String.valueOf(skill_tree_clone.charAt(i));
+                    if (!skill.contains(charSkill)) {
+                        skill_tree = skill_tree.replace(charSkill, "");
+                    }
                 }
-
+                if (skill.indexOf(skill_tree) == 0) {
+                    answer++;
+                }
+                index++;
+                if (index == skill_trees.length) break;
             }
-            if (skill.indexOf(skill_tree) == 0) {
-                answer++;
-            }
-            index++;
-            if (index == skill_trees.length) break;
+            return answer;
         }
-        System.out.println(answer);
     }
 }
+
+
