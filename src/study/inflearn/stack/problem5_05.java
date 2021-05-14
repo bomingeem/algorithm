a245fb85f094ba73b5101b2bb3616298075034c5
bomingeem <bomingeem@gmail.com>
2021-05-14 19:21:50 +0900

    [feat] 쇠막대기

---
+++ src/study/inflearn/stack/problem5_05.java
@@ -0,0 +1,32 @@
+package study.inflearn.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class problem5_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        problem5_05 problem = new problem5_05();
+        System.out.println(problem.solution(str));
+    }
+    public int solution(String str) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                stack.push('(');
+            } else if (str.charAt(i) == ')') {
+                if (str.charAt(i-1) == '(') {
+                    stack.pop();
+                    answer += stack.size();
+                } else {
+                    stack.pop();
+                    answer += 1;
+                }
+            }
+        }
+        return answer;
+    }
+}
