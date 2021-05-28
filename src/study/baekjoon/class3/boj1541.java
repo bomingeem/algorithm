cd1b72a1c08671d060c741b7df33a753d26bf8e9
bomingeem <bomingeem@gmail.com>
2021-05-28 13:38:50 +0900

    [feat] 잃어버린 괄호

--- src/study/baekjoon/class3/boj1541.java
+++ src/study/baekjoon/class3/boj1541.java
@@ -1,7 +1,6 @@
 package study.baekjoon.class3;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 public class boj1541 {
     public static void main(String[] args) {
@@ -10,20 +9,14 @@ public class boj1541 {
         String expression = scanner.next();
         int sum = Integer.MAX_VALUE;
         String[] sub = expression.split("-");
-        for (String s : sub) {
-            System.out.println("sub : " + s);
-        }
+
         //50, 50+40
         for (int i=0; i<sub.length; i++) {
             int number = 0;
             String[] add = sub[i].split("\\+");
-            for (String s : add) {
-                System.out.println("add : " + s);
-            }
 
             for (int j=0; j<add.length; j++) {
                 number += Integer.parseInt(add[j]);
-                System.out.println("number : " + number);
             }
 
             if (sum == Integer.MAX_VALUE) {
