ad8c45a03754dff0c3f717c3ba67cc85d294902e
bomingeem <bomingeem@gmail.com>
2021-10-11 16:47:41 +0900

    [feat] 큰 수 출력하기

---
+++ src/study/inflearn/array/problem2_01.java
@@ -0,0 +1,29 @@
+package study.inflearn.array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class problem2_01 {
+    public static void main(String[] args) {
+        problem2_01 problem = new problem2_01();
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+
+        for (int i=0; i<N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int x : problem.solution(N, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i=1; i<n; i++) {
+            if (arr[i] > arr[i-1]) answer.add(arr[i]);
+        }
+        return answer;
+    }
+}
