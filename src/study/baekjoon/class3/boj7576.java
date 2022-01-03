2053bc5d91bb3610d8462a2c109f577b0a7d6cc5
bomingeem <bomingeem@gmail.com>
2022-01-03 17:59:32 +0900

    [feat] 토마토 review

--- src/study/baekjoon/class3/boj7576.java
+++ src/study/baekjoon/class3/boj7576.java
@@ -1,46 +1,36 @@
 package study.baekjoon.class3;
 
-import sun.awt.image.ImageWatched;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class boj7576 {
-    //2이상 1000이하
-    static int M, N;
+    static int M; //상자의 가로 칸의 수
+    static int N; //상자의 세로 칸의 수
     static int[][] graph;
-    static int[] dx = {-1, 1, 0, 0}; //왼쪽 오른쪽
-    static int[] dy = {0, 0, -1, 1}; //앞 뒤
-
-    public static void main(String[] args) throws IOException {
-        //[백준 class3] 토마토
-        //토마토가 모두 익을 수 있는 최소 일수: BFS로 풀이
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); //세로
-        M = Integer.parseInt(st.nextToken()); //가로
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int day = 0;
+    public static void main(String[] args) {
+        //[백준] 토마토
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
         graph = new int[M][N];
+
         for (int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
             for (int j=0; j<N; j++) {
-                //토마토의 위치를 저장
-                graph[i][j] = Integer.parseInt(st.nextToken());
+                graph[i][j] = scanner.nextInt();
             }
         }
         BFS();
     }
 
-    static void BFS() {
-        Queue<Position> queue = new LinkedList<>();
-        int day = 0;
+    public static void BFS() {
+        Queue<Position> queue = new LinkedList<Position>();
 
         for (int i=0; i<M; i++) {
             for (int j=0; j<N; j++) {
-                //토마토가 있을 경우 큐에 삽입
                 if (graph[i][j] == 1) {
                     queue.offer(new Position(i, j, 0));
                 }
@@ -49,32 +39,32 @@ public class boj7576 {
 
         while (!queue.isEmpty()) {
             Position position = queue.poll();
-            day = position.pos;
+            day = position.day;
 
             for (int i=0; i<4; i++) {
-                int nx = position.x + dx[i];
-                int ny = position.y + dy[i];
+                int px = position.x + dx[i];
+                int py = position.y + dy[i];
 
-                if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
-                    if (graph[nx][ny] == 0) {
-                        graph[nx][ny] = 1;
-                        queue.add(new Position(nx, ny, day+1));
+                if (px >= 0 && px < M && py >= 0 && py < N) {
+                    if (graph[px][py] == 0) {
+                        graph[px][py] = 1;
+                        queue.offer(new Position(px, py, day+1));
                     }
                 }
             }
         }
-        if (checkAllTomato()) {
+
+        if (checkVariable()) {
             System.out.println(day);
         } else {
             System.out.println(-1);
         }
     }
 
-    static boolean checkAllTomato() {
-        for(int i=0; i<M; i++) {
+    public static boolean checkVariable() {
+        for (int i=0; i<M; i++) {
             for (int j=0; j<N; j++) {
                 if (graph[i][j] == 0) {
-                    //다 익지않은 상황
                     return false;
                 }
             }
@@ -82,16 +72,15 @@ public class boj7576 {
         return true;
     }
 
-    //토마토의 x, y, 위치를 저장하는 position static class
-    static class Position {
+    public static class Position {
         int x;
         int y;
-        int pos;
+        int day;
 
-        public Position(int x, int y, int pos){
+        public Position(int x, int y, int day) {
             this.x = x;
             this.y = y;
-            this.pos = pos;
+            this.day = day;
         }
     }
 }
