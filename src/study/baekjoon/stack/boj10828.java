f9b67a658f93e405c268f65fa192826298398192
bomingeem <bomingeem@gmail.com>
2022-01-10 22:36:19 +0900

    [feat] 스택 review

--- src/study/baekjoon/stack/boj10828.java
+++ src/study/baekjoon/stack/boj10828.java
@@ -3,41 +3,71 @@ package study.baekjoon.stack;
 import java.util.Scanner;
 
 public class boj10828 {
+    static int N;
+    static int size = 0;
+    static int[] stack;
     public static void main(String[] args) {
-        //구현 소스
+        //[백준] 스택
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] stack = new int[n];
-        int size = 0;
+        StringBuilder sb = new StringBuilder();
+        N = scanner.nextInt();
+        stack = new int[N];
 
-        while (n-- > 0) {
-            String word = scanner.next();
-            if (word.equals("push")) {
-                int num = Integer.parseInt(scanner.next());
-                stack[size] = num;
-                size++;
-            } else if (word.equals("pop")) {
-                if(size == 0) {
-                    System.out.println("-1");
-                } else {
-                    System.out.println(stack[size-1]);
-                    size = size-1;
-                }
-            } else if (word.equals("top")) {
-                if(size == 0) {
-                    System.out.println("-1");
-                } else {
-                    System.out.println(stack[size-1]);
-                }
-            } else if (word.equals("size")) {
-                System.out.println(size);
-            } else if (word.equals("empty")) {
-                if (size == 0) {
-                    System.out.println("1");
-                } else {
-                    System.out.println("0");
-                }
+        for (int i=0; i<N; i++) {
+            String command = scanner.next();
+            switch (command) {
+                case "push":
+                    push(scanner.nextInt());
+                    break;
+                case "pop":
+                    sb.append(pop()).append("\n");
+                    break;
+                case "size":
+                    sb.append(size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(empty()).append("\n");
+                    break;
+                case "top":
+                    sb.append(top()).append("\n");
+                    break;
             }
+        }
+        System.out.println(sb);
+    }
+
+    public static void push(int item) {
+        stack[size] = item;
+        size++;
+    }
+
+    public static int pop() {
+        if (size == 0) {
+            return -1;
+        } else {
+            int result = stack[size-1];
+            size--;
+            return result;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if (size == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int top() {
+        if (size == 0) {
+            return -1;
+        } else {
+            return stack[size-1];
         }
     }
 }
