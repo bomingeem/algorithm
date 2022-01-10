06cae251dcba68b7869ecce51943ba563125dac2
bomingeem <bomingeem@gmail.com>
2022-01-10 22:06:38 +0900

    [feat] 요세푸스 문제 review

--- src/study/baekjoon/queue/boj1158.java
+++ src/study/baekjoon/queue/boj1158.java
@@ -1,29 +1,28 @@
 package study.baekjoon.queue;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class boj1158 {
+    static int N;
+    static int K;
     public static void main(String[] args) {
-        //원에서 사람들이 제거되는 순서를 (N,K)-요세푸스 순열
-        //ex) (7,3) → <3,6,2,7,5,1,4>
+        //[백준] 요세푸스 문제
         Scanner scanner = new Scanner(System.in);
-        Queue<Integer> queue = new LinkedList<Integer>();
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        Queue<Integer> queue = new LinkedList<>();
+        N = scanner.nextInt();
+        K = scanner.nextInt();
         StringBuilder sb = new StringBuilder();
         sb.append("<");
 
-        for(int i=0; i<n; i++) {
-            queue.offer(i+1);
+        for (int i=1; i<=N; i++) {
+            queue.offer(i);
         }
 
-        for(int i=0; i<n-1; i++) {
-            for(int j=0; j<m-1; j++) {
+        while (queue.size() > 1) {
+            for (int i=0; i<K-1; i++) {
                 queue.offer(queue.poll());
             }
-            sb.append(queue.poll() + ", ");
+            sb.append(queue.poll()).append(", ");
         }
         sb.append(queue.poll() + ">");
         System.out.println(sb);
