d8a4166fb4d2c5b962338b7e9017cf8844e2c217
bomingeem <bomingeem@gmail.com>
2022-02-17 22:33:22 +0900

    [feat] 평균은 넘겠지

---
+++ src/study/baekjoon/math/boj4344.java
@@ -0,0 +1,38 @@
+package study.baekjoon.math;
+
+import java.util.Scanner;
+
+public class boj4344 {
+    static int C;
+    static int N;
+    static int[] arrays;
+    public static void main(String[] args) {
+        //[백준] 평균은 넘겠지
+        Scanner scanner = new Scanner(System.in);
+        C = scanner.nextInt();
+
+        for (int i=0; i<C; i++) {
+            N = scanner.nextInt();
+            arrays = new int[N];
+
+            double sum = 0;
+
+            for (int j=0; j<N; j++) {
+                int score = scanner.nextInt();
+                arrays[j] = score;
+                sum += score;
+            }
+
+            double average = (sum / N);
+            double count = 0;
+
+            for (int k=0; k<N; k++) {
+                if (arrays[k] > average) {
+                    count++;
+                }
+            }
+
+            System.out.println((count/N) * 100);
+        }
+    }
+}
