3bbd57c6e31efce0b199b1cea3d2ddd7b23077c5
bomingeem <bomingeem@gmail.com>
2022-03-01 15:16:03 +0900

    [feat] 차이를 최대로

--- src/study/baekjoon/bruteforce/boj10819.java
+++ src/study/baekjoon/bruteforce/boj10819.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class boj10819 {
     static int N;
-    static int[] A, temp;
+    static int[] A;
+    static int[] temporaryA;
     static boolean[] visited;
     static int result = 0;
     public static void main(String[] args) {
@@ -12,31 +13,34 @@ public class boj10819 {
         Scanner scanner = new Scanner(System.in);
         N = scanner.nextInt();
         A = new int[N];
-        temp = new int[N];
+        temporaryA = new int[N]; //각 순열 케이스를 담아줄 임시 저장소
         visited = new boolean[N];
 
         for (int i=0; i<N; i++) {
             A[i] = scanner.nextInt();
         }
+
         permutation(0);
         System.out.println(result);
     }
 
-    static void permutation(int index) {
-        if (index == N) {
+    public static void permutation(int count) {
+        if (count == N) {
             int sum = 0;
             for (int i=0; i<N-1; i++) {
-                sum += Math.abs(temp[i] - temp[i + 1]);
+                sum += Math.abs(temporaryA[i] - temporaryA[i+1]);
             }
             result = Math.max(result, sum);
             return;
         }
 
         for (int i=0; i<N; i++) {
-            if (visited[i] == true) continue;
+            if (visited[i]) {
+                continue;
+            }
             visited[i] = true;
-            temp[index] = A[i];
-            permutation(index + 1);
+            temporaryA[count] = A[i];
+            permutation(count+1);
             visited[i] = false;
         }
     }
