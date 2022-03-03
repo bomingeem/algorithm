348f8e1e7d959bff3705f6ddaf555d40fb3b3231
bomingeem <bomingeem@gmail.com>
2022-03-03 21:50:54 +0900

    [feat] 일곱 난쟁이

--- src/study/baekjoon/bruteforce/boj2309.java
+++ src/study/baekjoon/bruteforce/boj2309.java
@@ -4,34 +4,35 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class boj2309 {
-    static int[] dwarf = new int[9];
-    static boolean check = false;
+    static int[] arr = new int[9];
+    static boolean visited = false;
     static int sum = 0;
     public static void main(String[] args) {
         //[백준] 일곱 난쟁이
         Scanner scanner = new Scanner(System.in);
 
-        for (int i=0; i<dwarf.length; i++) {
-            dwarf[i] = scanner.nextInt();
-            sum += dwarf[i];
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
         }
 
-        for (int i=0; i<dwarf.length; i++) {
-            if (check) break;
-            for (int j=0; j<dwarf.length; j++) {
+        for (int i=0; i<arr.length; i++) {
+            if (visited) break;
+            for (int j=0; j<arr.length; j++) {
                 if (i == j) continue;
-                if (sum - dwarf[i] - dwarf[j] == 100) {
-                    dwarf[i] = 0;
-                    dwarf[j] = 0;
-                    check = true;
+                if (sum - arr[i] - arr[j] == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    visited = true;
                     break;
                 }
             }
         }
-        Arrays.sort(dwarf);
 
-        for (int i=0; i<dwarf.length; i++) {
-            if (dwarf[i] != 0) System.out.println(dwarf[i]);
+        Arrays.sort(arr);
+
+        for (int i : arr) {
+            if (i != 0) System.out.println(i);
         }
     }
 }
