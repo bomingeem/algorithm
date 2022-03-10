1c7b3e38274c8249dda782405595c582f9c1a509
bomingeem <bomingeem@gmail.com>
2022-03-10 23:19:04 +0900

    [feat] 미로탐색

--- src/study/baekjoon/graphTraversal/boj2178.java
+++ src/study/baekjoon/graphTraversal/boj2178.java
@@ -23,9 +23,9 @@ public class boj2178 {
         visited = new int[N][M];
 
         for (int i=0; i<N; i++) {
-            String str = br.readLine();
+            String[] str = br.readLine().split("");
             for (int j=0; j<M; j++) {
-                graph[i][j] = str.charAt(j) - '0';
+                graph[i][j] = Integer.parseInt(str[j]);
             }
         }
         BFS(0, 0);
@@ -33,32 +33,34 @@ public class boj2178 {
     }
 
     static void BFS(int x, int y) {
-        Queue<Maze> queue = new LinkedList<Maze>();
+        Queue<Coordinate> queue = new LinkedList<>();
         visited[x][y] = 1;
-        queue.offer(new Maze(x, y));
+        queue.offer(new Coordinate(x, y));
 
         while (!queue.isEmpty()) {
-            Maze maze = queue.poll();
+            Coordinate coordinate = queue.poll();
 
             for (int i=0; i<4; i++) {
-                int mx = maze.x + dx[i];
-                int my = maze.y + dy[i];
+                int mx = coordinate.x + dx[i];
+                int my = coordinate.y + dy[i];
 
                 if (mx >= 0 && mx < N && my >= 0 && my < M) {
                     if (graph[mx][my] == 1 && visited[mx][my] == 0) {
-                        queue.offer(new Maze(mx, my));
-                        visited[mx][my] = visited[maze.x][maze.y] + 1;
+                        queue.offer(new Coordinate(mx, my));
+                        //탐색 시, 다음에 밟을 칸에 현재 칸까지의 가중치를 더해주면 된다.
+                        //visited[다음칸.x][다음칸.y] = visited[이전칸.x][이전칸.y] + 1;
+                        visited[mx][my] = visited[coordinate.x][coordinate.y] + 1;
                     }
                 }
             }
         }
     }
 
-    public static class Maze {
+    public static class Coordinate {
         int x;
         int y;
 
-        public Maze(int x, int y) {
+        public Coordinate(int x, int y) {
             this.x = x;
             this.y = y;
         }
