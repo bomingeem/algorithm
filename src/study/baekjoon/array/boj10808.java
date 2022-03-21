c39c7ce0af23dba2a15aba283d17a2c611f9b5f0
bomingeem <bomingeem@gmail.com>
2022-03-21 21:45:44 +0900

    [fix] 알파벳 개수

--- src/study/baekjoon/array/boj10808.java
+++ src/study/baekjoon/array/boj10808.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class boj10808 {
     static String S;
-    static int[] frequency = new int[26];
+    static int[] alphabet = new int[26];
     public static void main(String[] args) {
         //[백준] 알파벳 개수
         Scanner scanner = new Scanner(System.in);
         S = scanner.next();
 
         for (char ch : S.toCharArray()) {
-            frequency[ch-'a']++;
+            alphabet[ch - 'a']++;
         }
 
-        for (int i=0; i<26; i++) {
-            System.out.print(frequency[i] + " ");
+        for (int i : alphabet) {
+            System.out.print(i + " ");
         }
     }
 }
