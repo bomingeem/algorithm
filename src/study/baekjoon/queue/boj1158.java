8bbae5bf7ba6052b88f17975b2378912f608b19b
bomingeem <bomingeem@gmail.com>
2022-04-03 13:42:37 +0900

    [feat] 요세푸스 문제

--- src/study/baekjoon/queue/boj1158.java
+++ src/study/baekjoon/queue/boj1158.java
@@ -1,30 +1,35 @@
 package study.baekjoon.queue;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class boj1158 {
     static int N;
     static int K;
-    public static void main(String[] args) {
+    static Queue<Integer> queue = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
         //[백준] 요세푸스 문제
-        Scanner scanner = new Scanner(System.in);
-        Queue<Integer> queue = new LinkedList<>();
-        N = scanner.nextInt();
-        K = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        sb.append("<");
-
-        for (int i=1; i<=N; i++) {
-            queue.offer(i);
+        String[] commands = br.readLine().split(" ");
+        N = Integer.parseInt(commands[0]);
+        K = Integer.parseInt(commands[1]);
+        for (int i=0; i<N; i++) {
+            queue.offer(i+1);
         }
 
+        sb.append("<");
         while (queue.size() > 1) {
             for (int i=0; i<K-1; i++) {
                 queue.offer(queue.poll());
             }
             sb.append(queue.poll()).append(", ");
         }
-        sb.append(queue.poll() + ">");
+
+        sb.append(queue.poll()).append(">");
         System.out.println(sb);
     }
 }
