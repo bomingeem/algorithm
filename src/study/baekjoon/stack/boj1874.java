45075505a52f4b982d66b0498dd22b8a63e954ee
bomingeem <bomingeem@gmail.com>
2022-04-11 23:57:40 +0900

    [feat] 스택수열

--- src/study/baekjoon/stack/boj1874.java
+++ src/study/baekjoon/stack/boj1874.java
@@ -1,29 +1,27 @@
 package study.baekjoon.stack;
 
-import java.util.ArrayList;
+
 import java.util.Scanner;
 import java.util.Stack;
 
 public class boj1874 {
+    static int n;
+    static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args) {
         //[백준] 스택수열
         Scanner scanner = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
+        n = scanner.nextInt();
 
-        int N = scanner.nextInt();
-        int start = 0;
+        int count = 1;
 
-        while (N-- > 0) {
-            int value = scanner.nextInt();
-
-            if (value > start) {
-                for (int i=start+1; i<=value; i++) {
-                    stack.push(i);
-                    sb.append("+").append("\n");
-                }
-                start = value;
-            } else if (stack.peek() != value) {
+        while (n-- > 0) {
+            int number = scanner.nextInt();
+            while (count <= number) {
+                stack.push(count++);
+                sb.append("+").append("\n");
+            }
+            if (stack.peek() != number) {
                 System.out.println("NO");
                 return;
             }
