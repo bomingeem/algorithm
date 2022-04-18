a049f32fce725b83402cb6d3aaa02e32dccb7d91
bomingeem <bomingeem@gmail.com>
2022-04-18 22:43:53 +0900

    [feat] 큐

--- src/study/baekjoon/queue/boj10845.java
+++ src/study/baekjoon/queue/boj10845.java
@@ -3,53 +3,89 @@ package study.baekjoon.queue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class boj10845 {
+    //[백준] 큐
+    static final int MAX = 1000005;
+    static Queue<Integer> queue = new LinkedList<>();
     public static void main(String[] args) throws IOException {
-        //시간초과
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] queue = new int[n];
-        int begin = 0;
-        int end = 0;
-        scanner.nextLine();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer command;
+        StringBuilder sb = new StringBuilder();
+        int number = Integer.parseInt(br.readLine());
+        int back = -1;
 
-        while(n-- > 0) {
-            String command = scanner.next();
-            if(command.equals("push")) {
-                int num = Integer.parseInt(scanner.next());
-                queue[end] = num;
-                end += 1;
-            } else if(command.equals("pop")) {
-                if (begin == end) {
-                    System.out.println("-1");
-                } else {
-                    System.out.println(queue[begin]);
-                    begin += 1;
-                }
-            } else if(command.equals("size")) {
-                System.out.println(end-begin);
-            } else if(command.equals("empty")) {
-                if (begin == end) {
-                    System.out.println("1");
-                } else {
-                    System.out.println("0");
-                }
-            } else if(command.equals("front")) {
-                if(begin == end) {
-                    System.out.println("-1");
-                } else {
-                    System.out.println(queue[begin]);
-                }
-
-            } else if(command.equals("back")) {
-                if(begin == end) {
-                    System.out.println("-1");
-                } else {
-                    System.out.println(queue[end-1]);
-                }
+        while (number-- > 0) {
+            command = new StringTokenizer(br.readLine(), " ");
+            switch (command.nextToken()) {
+                case "push":
+                    back = Integer.parseInt(command.nextToken());
+                    queue.offer(back);
+                    break;
+                case "pop":
+                    sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+                    break;
+                case "back":
+                    sb.append(queue.isEmpty() ? -1 : back).append("\n");
+                    break;
+                default:
+                    break;
             }
         }
+        System.out.println(sb.toString());
     }
+
+    /*
+    public static void push(int x) {
+        queue[++tail] = x;
+    }
+
+    public static int pop() {
+        if (size() == 0) {
+            return -1;
+        } else {
+            return queue[head++];
+        }
+    }
+
+    public static int size() {
+        return tail-head+1;
+    }
+
+    public static int empty() {
+        if (size() == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int front() {
+        if (size() == 0) {
+            return -1;
+        } else {
+            return queue[head];
+        }
+    }
+
+    public static int back() {
+        if (size() == 0) {
+            return -1;
+        } else {
+            return queue[tail];
+        }
+    }
+    */
 }
