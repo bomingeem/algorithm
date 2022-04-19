91c61907b4df8e65a3d524513d837330ad40fb55
bomingeem <bomingeem@gmail.com>
2022-04-19 22:04:47 +0900

    [feat] 카드2

--- src/study/baekjoon/queue/boj2164.java
+++ src/study/baekjoon/queue/boj2164.java
@@ -1,26 +1,27 @@
 package study.baekjoon.queue;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class boj2164 {
     static int N;
-    static Queue<Integer> queue = new LinkedList<Integer>();
-    public static void main(String[] args) {
+    static Queue<Integer> queue = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
         //[백준] 카드2
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-        for (int i=1; i<=N; i++) {
-            queue.offer(i);
+        for (int i=0; i<N; i++) {
+            queue.offer(i + 1);
         }
 
         while (queue.size() > 1) {
             queue.poll();
             queue.offer(queue.poll());
         }
-
-        System.out.println(queue.poll());
+        System.out.println(queue.peek());
     }
 }
