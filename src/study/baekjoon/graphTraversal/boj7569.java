8ffe2f25ba181785baf3980fc494dbc70d57f162
bomingeem <bomingeem@gmail.com>
2022-05-26 22:14:08 +0900

    [feat] 토마토

--- src/study/baekjoon/graphTraversal/boj7569.java
+++ src/study/baekjoon/graphTraversal/boj7569.java
@@ -1,72 +1,78 @@
 package study.baekjoon.graphTraversal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class boj7569 {
-    static int M, N, H; //가로, 세로, 높이
+    static int M, N, H;
     static int[][][] graph;
     static int day = 0;
     static int[] dx = {-1, 1, 0, 0, 0, 0};
     static int[] dy = {0, 0, -1, 1, 0, 0};
     static int[] dz = {0, 0, 0, 0, -1, 1};
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //[백준] 토마토
-        Scanner scanner = new Scanner(System.in);
-        M = scanner.nextInt();
-        N = scanner.nextInt();
-        H = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
         graph = new int[H][N][M];
 
         for (int i=0; i<H; i++) {
             for (int j=0; j<N; j++) {
+                st = new StringTokenizer(br.readLine());
                 for (int k=0; k<M; k++) {
-                    graph[i][j][k] = scanner.nextInt();
+                    graph[i][j][k] = Integer.parseInt(st.nextToken());
                 }
             }
         }
         BFS();
     }
 
-    static void BFS() {
-        Queue<Position> queue = new LinkedList<Position>();
+    public static void BFS() {
+        Queue<Tomato> queue = new LinkedList<>();
 
         for (int i=0; i<H; i++) {
             for (int j=0; j<N; j++) {
                 for (int k=0; k<M; k++) {
                     if (graph[i][j][k] == 1) {
-                        queue.offer(new Position(i, j, k, day));
+                        queue.offer(new Tomato(i, j, k, day));
                     }
                 }
             }
         }
 
         while (!queue.isEmpty()) {
-            Position position = queue.poll();
-            day = position.day;
+            Tomato tomato = queue.poll();
+            day = tomato.day;
 
             for (int i=0; i<6; i++) {
-                int px = position.x + dx[i];
-                int py = position.y + dy[i];
-                int pz = position.z + dz[i];
+                int tx = tomato.x + dx[i];
+                int ty = tomato.y + dy[i];
+                int tz = tomato.z + dz[i];
 
-                if (px >= 0 && px < N && py >= 0 && py < M && pz >= 0 && pz < H) {
-                    if (graph[pz][px][py] == 0) {
-                        graph[pz][px][py] = 1;
-                        queue.offer(new Position(pz, px, py, day+1));
+                if (tx >= 0 && tx < N && ty >= 0 && ty < M && tz >= 0 && tz < H) {
+                    if (graph[tz][tx][ty] == 0) {
+                        graph[tz][tx][ty] = 1;
+                        queue.offer(new Tomato(tz, tx, ty, day+1));
                     }
                 }
             }
         }
-        if (checkVariable()) {
+
+        if (checkAllTomato()) {
             System.out.println(day);
         } else {
             System.out.println(-1);
         }
     }
 
-    static boolean checkVariable() {
+    public static boolean checkAllTomato() {
         for (int i=0; i<H; i++) {
             for (int j=0; j<N; j++) {
                 for (int k=0; k<M; k++) {
@@ -79,13 +85,13 @@ public class boj7569 {
         return true;
     }
 
-    static class Position {
-        int z;
+    public static class Tomato {
         int x;
         int y;
+        int z;
         int day;
 
-        public Position(int z, int x, int y, int day) {
+        public Tomato(int z, int x, int y, int day) {
             this.z = z;
             this.x = x;
             this.y = y;
