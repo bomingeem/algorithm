964f58665c3e13ada4ee4cc7f9dcb8e0642b054d
bomingeem <bomingeem@gmail.com>
2022-06-20 21:57:35 +0900

    [feat] 개

---
+++ src/study/baekjoon/step1/boj10172.java
@@ -0,0 +1,12 @@
+package study.baekjoon.step1;
+
+public class boj10172 {
+    public static void main(String[] args) {
+        //[백준] 개
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
