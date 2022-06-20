359a6f2ab786ed4308a9170fee54384a3da26f1f
bomingeem <bomingeem@gmail.com>
2022-06-20 22:13:53 +0900

    [feat] ??!

---
+++ src/study/baekjoon/step1/boj10926.java
@@ -0,0 +1,12 @@
+package study.baekjoon.step1;
+
+import java.util.Scanner;
+
+public class boj10926 {
+    public static void main(String[] args) {
+        //[백준] ??!
+        Scanner scanner = new Scanner(System.in);
+        String joonha = scanner.next();
+        System.out.println(joonha + "??!");
+    }
+}
