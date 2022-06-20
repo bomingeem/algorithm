188cbfe5d15c3831cddb41ff49652a1db646b633
bomingeem <bomingeem@gmail.com>
2022-06-20 22:05:37 +0900

    [feat] AxB

---
+++ src/study/baekjoon/step1/boj10998.java
@@ -0,0 +1,17 @@
+package study.baekjoon.step1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj10998 {
+    public static void main(String[] args) throws IOException {
+        //[백준] AxB
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println(A*B);
+    }
+}
