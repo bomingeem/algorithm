651c1a73327e4da2e0622202f570226a3963f45a
bomingeem <bomingeem@gmail.com>
2022-06-20 21:59:53 +0900

    [feat] A+B

---
+++ src/study/baekjoon/step1/boj1000.java
@@ -0,0 +1,13 @@
+package study.baekjoon.step1;
+
+import java.util.Scanner;
+
+public class boj1000 {
+    public static void main(String[] args) {
+        //[백준] A+B
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A+B);
+    }
+}
