b614e647effd9470fd1572b2effb2dc4722bb137
bomingeem <bomingeem@gmail.com>
2022-06-20 21:50:34 +0900

    [feat] Hello World

--- src/study/baekjoon/step1/boj2557.java
+++ src/study/baekjoon/step1/boj2557.java
@@ -2,6 +2,7 @@ package study.baekjoon.step1;
 
 public class boj2557 {
     public static void main(String[] args) {
+        //[백준] Hello World
         System.out.println("Hello World!");
     }
 }
