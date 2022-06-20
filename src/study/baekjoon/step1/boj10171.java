48e721f0ec888a737460117c08afb9a1ec617275
bomingeem <bomingeem@gmail.com>
2022-06-20 21:54:04 +0900

    [feat] 고양이

---
+++ src/study/baekjoon/step1/boj10171.java
@@ -0,0 +1,11 @@
+package study.baekjoon.step1;
+
+public class boj10171 {
+    public static void main(String[] args) {
+        //[백준] 고양이
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
