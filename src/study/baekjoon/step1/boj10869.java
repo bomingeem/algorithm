205bb9991f1f13130767679bb59f1dcf0a17bffa
bomingeem <bomingeem@gmail.com>
2022-06-20 22:11:24 +0900

    [feat] 사칙연산

---
+++ src/study/baekjoon/step1/boj10869.java
@@ -0,0 +1,17 @@
+package study.baekjoon.step1;
+
+import java.util.Scanner;
+
+public class boj10869 {
+    public static void main(String[] args) {
+        //[백준] 사칙연산
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
