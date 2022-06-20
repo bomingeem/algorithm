2532e41b07ca334927259e8b9df849ce50cc1b27
bomingeem <bomingeem@gmail.com>
2022-06-20 21:52:20 +0900

    [feat] We love kriii

---
+++ src/study/baekjoon/step1/boj10718.java
@@ -0,0 +1,10 @@
+package study.baekjoon.step1;
+
+public class boj10718 {
+    public static void main(String[] args) {
+        //[백준] We love kriii
+        for (int i=0; i<2; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
