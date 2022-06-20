497720bccd26b60001eefe45145e14202496b26d
bomingeem <bomingeem@gmail.com>
2022-06-20 22:16:33 +0900

    [feat] 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/study/baekjoon/step1/boj18108.java
@@ -0,0 +1,12 @@
+package study.baekjoon.step1;
+
+import java.util.Scanner;
+
+public class boj18108 {
+    public static void main(String[] args) {
+        //[백준] 1998년생인 내가 태국에서는 2541년생?!
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        System.out.println(year - 543);
+    }
+}
