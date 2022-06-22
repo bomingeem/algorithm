f986bf5728cae1e42900cc1d2938d4d5890aa8a2
bomingeem <bomingeem@gmail.com>
2022-06-22 22:47:51 +0900

    [feat] N 찍기

---
+++ src/study/baekjoon/step3/boj2741.java
@@ -0,0 +1,15 @@
+package study.baekjoon.step3;
+
+import java.util.Scanner;
+
+public class boj2741 {
+    public static void main(String[] args) {
+        //[백준] N 찍기
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i=0; i<N; i++) {
+            System.out.println(i+1);
+        }
+    }
+}
