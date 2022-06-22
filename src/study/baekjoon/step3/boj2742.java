bc5d7af1fdcfb2c3c140ba06b46ffaeeaf5144e7
bomingeem <bomingeem@gmail.com>
2022-06-22 22:50:17 +0900

    [feat] 기찍 N

---
+++ src/study/baekjoon/step3/boj2742.java
@@ -0,0 +1,15 @@
+package study.baekjoon.step3;
+
+import java.util.Scanner;
+
+public class boj2742 {
+    public static void main(String[] args) {
+        //[백준] 기찍 N
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i=N; i>0; i--) {
+            System.out.println(i);
+        }
+    }
+}
