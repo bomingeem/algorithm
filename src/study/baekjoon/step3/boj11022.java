d2e12e80d35997d2a5890eae8c5010c27bedbbfc
bomingeem <bomingeem@gmail.com>
2022-06-22 22:54:54 +0900

    [feat] A+B - 8

---
+++ src/study/baekjoon/step3/boj11022.java
@@ -0,0 +1,17 @@
+package study.baekjoon.step3;
+
+import java.util.Scanner;
+
+public class boj11022 {
+    public static void main(String[] args) {
+        //[백준] A+B - 8
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        for (int i=0; i<T; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.println("Case #"+(i+1)+": "+A+" + "+B+" = "+(A+B));
+        }
+    }
+}
