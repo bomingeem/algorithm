7ed5f25bd131dc09235872a45e6675ed2c3d34ca
bomingeem <bomingeem@gmail.com>
2022-07-02 15:03:02 +0900

    [feat] 아스키 코드

---
+++ src/study/baekjoon/step6/boj11654.java
@@ -0,0 +1,12 @@
+package study.baekjoon.step6;
+
+import java.util.Scanner;
+
+public class boj11654 {
+    public static void main(String[] args) {
+        //[백준] 아스키 코드
+        Scanner scanner = new Scanner(System.in);
+        int ch = scanner.next().charAt(0);
+        System.out.println(ch);
+    }
+}
