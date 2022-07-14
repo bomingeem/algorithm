ef396ea21a2b38945a0eef534959729b4f2759e8
bomingeem <bomingeem@gmail.com>
2022-07-14 10:47:18 +0900

    [feat] 피보나치 수 5

--- src/study/baekjoon/step9/boj10870.java
+++ src/study/baekjoon/step9/boj10870.java
@@ -1,4 +1,4 @@
-package study.baekjoon.recursion;
+package study.baekjoon.step9;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,17 +8,17 @@ public class boj10870 {
     public static void main(String[] args) throws IOException {
         //[백준] 피보나치 수 5
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int N = Integer.parseInt(str);
-        System.out.println(fibonacci(N));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
     }
 
     public static int fibonacci(int n) {
-        //f(n) = f(n-1) + f(n-2)
-        if (n == 0) return 0;
-        else if (n == 1) return 1;
-        else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
         }
     }
 }
