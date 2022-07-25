7dc7e22f8601a3393093645cb2e49bf4151ae7d7
bomingeem <bomingeem@gmail.com>
2022-07-25 10:56:20 +0900

    [fix] 수 정렬하기 2: counting sort로 풀이

--- src/study/baekjoon/step11/boj2751.java
+++ src/study/baekjoon/step11/boj2751.java
@@ -3,9 +3,6 @@ package study.baekjoon.step11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class boj2751 {
     public static void main(String[] args) throws IOException {
@@ -13,18 +10,18 @@ public class boj2751 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        List<Integer> integers = new ArrayList<>();
+
+        boolean[] arr = new boolean[2000001];
 
         for (int i=0; i<N; i++) {
-            integers.add(Integer.parseInt(br.readLine()));
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
         }
 
-        Collections.sort(integers);
-
-        for (Integer number : integers) {
-            sb.append(number).append("\n");
+        for (int i=0; i<2000001; i++) {
+            if (arr[i]) {
+                sb.append(i - 1000000).append("\n");
+            }
         }
-
         System.out.println(sb);
     }
 }
