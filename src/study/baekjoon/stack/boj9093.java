b3ddfcad740ddbb95b6662d781ee6e9dd1927ff1
bomingeem <bomingeem@gmail.com>
2022-08-02 11:15:49 +0900

    [feat] 단어 뒤집기

--- src/study/baekjoon/stack/boj9093.java
+++ src/study/baekjoon/stack/boj9093.java
@@ -1,32 +1,32 @@
 package study.baekjoon.stack;
 
-import java.util.Scanner;
+
+import java.io.*;
 import java.util.Stack;
 
 public class boj9093 {
-    public static void main(String[] args) {
-        //시간초과나네 왜지?
-        //Scanner.nextInt 메소드는 사용자 입력의 가장 마지막 개행문자를 제거하지 않음
-        //개행문자 전까지만 숫자로 입력받아 다음에 호출되는 Scanner.nextLine() 메소드의 입력으로 처리되서 문제가 발생
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        Stack<Character> reverse = new Stack<Character>();
-        scanner.nextLine(); //개행문자(Enter)를 제거하기 위해 추가
+    static int T;
+    static Stack<Character> stack = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        //[백준] 단어 뒤집기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        T = Integer.parseInt(br.readLine());
 
-        while (size-- > 0) {
-            String word = scanner.nextLine() + " ";
+        while (T-- > 0) {
+            String word = br.readLine() + "\n";
 
-            for(int i=0; i<word.length(); i++) {
-                if (word.charAt(i) == ' ') {
-                    while(!reverse.empty()) {
-                        System.out.print(reverse.pop());
+            for (char ch : word.toCharArray()) {
+                if (ch == ' ' || ch == '\n') {
+                    while (!stack.isEmpty()) {
+                        bw.write(stack.pop());
                     }
-                    System.out.print(" ");
+                    bw.write(ch);
                 } else {
-                    reverse.push(word.charAt(i));
+                    stack.push(ch);
                 }
             }
-            System.out.println();
         }
+        bw.flush();
     }
 }
