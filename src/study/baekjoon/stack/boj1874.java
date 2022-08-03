ed751872909fbaba9424e61cce45a1afdf6a9a82
bomingeem <bomingeem@gmail.com>
2022-08-03 11:19:30 +0900

    [feat] 스택 수열

--- src/study/baekjoon/stack/boj1874.java
+++ src/study/baekjoon/stack/boj1874.java
@@ -1,32 +1,36 @@
 package study.baekjoon.stack;
 
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class boj1874 {
     static int n;
     static Stack<Integer> stack = new Stack<>();
-    public static void main(String[] args) {
-        //[백준] 스택수열
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        //[백준] 스택 수열
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        n = scanner.nextInt();
+        n = Integer.parseInt(br.readLine());
 
         int count = 1;
 
         while (n-- > 0) {
-            int number = scanner.nextInt();
+            int number = Integer.parseInt(br.readLine());
             while (count <= number) {
-                stack.push(count++);
+                stack.push(count);
+                count++;
                 sb.append("+").append("\n");
             }
-            if (stack.peek() != number) {
+
+            if (stack.peek() == number) {
+                stack.pop();
+                sb.append("-").append("\n");
+            } else {
                 System.out.println("NO");
                 return;
             }
-            stack.pop();
-            sb.append("-").append("\n");
         }
         System.out.println(sb);
     }
