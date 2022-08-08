1a1b7d03cb9988eea5c934fe43fb9443e3c21d52
bomingeem <bomingeem@gmail.com>
2022-08-08 11:03:49 +0900

    [feat] 덱

--- src/study/baekjoon/queue/boj10866.java
+++ src/study/baekjoon/queue/boj10866.java
@@ -8,25 +8,23 @@ import java.util.StringTokenizer;
 public class boj10866 {
     static int N;
     static int[] deque;
-    static int head, tail;
+    static int start, end;
     public static void main(String[] args) throws IOException {
         //[백준] 덱
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer command;
         N = Integer.parseInt(br.readLine());
-
         deque = new int[N];
-        head = tail = N/2;
+        start = end = N/2;
 
         for (int i=0; i<N; i++) {
-            command = new StringTokenizer(br.readLine(), " ");
-            switch (command.nextToken()) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            switch (st.nextToken()) {
                 case "push_front":
-                    push_front(Integer.parseInt(command.nextToken()));
+                    push_front(Integer.parseInt(st.nextToken()));
                     break;
                 case "push_back":
-                    push_back(Integer.parseInt(command.nextToken()));
+                    push_back(Integer.parseInt(st.nextToken()));
                     break;
                 case "pop_front":
                     sb.append(pop_front()).append("\n");
@@ -54,41 +52,41 @@ public class boj10866 {
     }
 
     public static void push_front(int x) {
-        deque[head] = x;
-        head--;
+        deque[start] = x;
+        start--;
     }
 
     public static void push_back(int x) {
-        tail++;
-        deque[tail] = x;
+        end++;
+        deque[end] = x;
     }
 
     public static int pop_front() {
-        if (head == tail) {
+        if (start == end) {
             return -1;
         } else {
-            int result = deque[head + 1];
-            head++;
+            int result = deque[start + 1];
+            start++;
             return result;
         }
     }
 
     public static int pop_back() {
-        if (head == tail) {
+        if (start == end) {
             return -1;
         } else {
-            int result = deque[tail];
-            tail--;
+            int result = deque[end];
+            end--;
             return result;
         }
     }
 
     public static int size() {
-        return tail - head;
+        return end - start;
     }
 
     public static int empty() {
-        if (head == tail) {
+        if (start == end) {
             return 1;
         } else {
             return 0;
@@ -96,18 +94,18 @@ public class boj10866 {
     }
 
     public static int front() {
-        if (head == tail) {
+        if (start == end) {
             return -1;
         } else {
-            return deque[head + 1];
+            return deque[start + 1];
         }
     }
 
     public static int back() {
-        if (head == tail) {
+        if (start == end) {
             return -1;
         } else {
-            return deque[tail];
+            return deque[end];
         }
     }
 }
