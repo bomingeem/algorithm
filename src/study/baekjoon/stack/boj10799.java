196981329e1a6a0408b605011032784fab9d5b57
bomingeem <bomingeem@gmail.com>
2022-08-10 14:49:19 +0900

    [feat] 쇠막대기

--- src/study/baekjoon/stack/boj10799.java
+++ src/study/baekjoon/stack/boj10799.java
@@ -1,21 +1,23 @@
 package study.baekjoon.stack;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class boj10799 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int count = 0;
-        String bracket = scanner.nextLine();
-        Stack<String> stack = new Stack<String>();
+    static int count = 0;
+    static Stack<String> stack = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        //[백준] 쇠막대기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String bracket = br.readLine();
 
-        for(int i=0; i<bracket.length(); i++) {
-
+        for (int i=0; i<bracket.length(); i++) {
             if (bracket.charAt(i) == '(') {
                 stack.push("(");
             } else if (bracket.charAt(i) == ')') {
-                if(bracket.charAt(i-1) == '(') { //레이저
+                if (bracket.charAt(i-1) == '(') {
                     stack.pop();
                     count += stack.size();
                 } else {
