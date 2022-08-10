c45bdab6b53819c16a2cfe771b9d3ee35744c24c
bomingeem <bomingeem@gmail.com>
2022-08-10 14:36:05 +0900

    [feat] 단어 뒤집기 2

--- src/study/baekjoon/stack/boj17413.java
+++ src/study/baekjoon/stack/boj17413.java
@@ -1,37 +1,36 @@
 package study.baekjoon.stack;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class boj17413 {
+    static String S;
+    static boolean tag;
+    static Stack<Character> stack = new Stack<>();
     public static void main(String[] args) throws IOException {
+        //[백준] 단어 뒤집기 2
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        Stack<Character> stack = new Stack<Character>();
-        boolean tag = false;
-        String s = br.readLine();
+        S = br.readLine();
 
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '<') {
+        for (int i=0; i<S.length(); i++) {
+            char ch = S.charAt(i);
+            if(ch == '<') {
                 print(stack);
                 tag = true;
-                System.out.print(c);
-            } else if (c == '>') {
+                System.out.print(ch);
+            } else if (ch == '>') {
                 tag = false;
-                System.out.print(c);
+                System.out.print(ch);
             } else if (tag) {
-                System.out.print(c);
+                System.out.print(ch);
             } else {
-                if (c == ' ') {
+                if (ch == ' ') {
                     print(stack);
-                    System.out.print(" ");
+                    System.out.print(ch);
                 } else {
-                    stack.push(c);
+                    stack.push(ch);
                 }
             }
         }
@@ -39,7 +38,7 @@ public class boj17413 {
     }
 
     static void print(Stack stack) {
-        while (!stack.empty()) {
+        while (!stack.isEmpty()) {
             System.out.print(stack.peek());
             stack.pop();
         }
