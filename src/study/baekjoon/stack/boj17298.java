eb3b74408d92b5d15efe71b741d7d87deaf51177
bomingeem <bomingeem@gmail.com>
2022-08-11 10:18:21 +0900

    [feat] 오큰수

--- src/study/baekjoon/stack/boj17298.java
+++ src/study/baekjoon/stack/boj17298.java
@@ -1,39 +1,42 @@
 package study.baekjoon.stack;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class boj17298 {
-    public static void main(String[] args) {
-        //오큰수(NGE) 구하기
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        int[] nge = new int[n];
-        Stack<Integer> stack = new Stack<Integer>();
+    static int A;
+    static int[] seq;
+    static Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        //[백준] 오큰수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        A = Integer.parseInt(br.readLine());
+        seq = new int[A];
 
-        for(int i=0; i<n; i++) {
-            arr[i] = scanner.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i=0; i<A; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
         }
 
-        stack.push(0);
-        for(int i=1; i<arr.length; i++) {
-            if(stack.isEmpty()) {
-                stack.push(i);
-            }
-            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-                nge[stack.pop()] = arr[i];
+        for (int i=0; i<A; i++) {
+            while (!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
+                seq[stack.pop()] = seq[i];
             }
             stack.push(i);
         }
 
         while (!stack.isEmpty()) {
-            nge[stack.pop()] = -1;
+            seq[stack.pop()] = -1;
         }
-        for(int i=0; i<nge.length; i++) {
-            sb.append(nge[i] + " ");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<A; i++) {
+            sb.append(seq[i]).append(" ");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
