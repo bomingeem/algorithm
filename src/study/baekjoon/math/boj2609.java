981aa0f29b5ded925dcc320ba8ee3fa53f5aa7d5
bomingeem <bomingeem@gmail.com>
2022-08-13 15:08:52 +0900

    [feat] 최대공약수와 최대공배수

--- src/study/baekjoon/math/boj2609.java
+++ src/study/baekjoon/math/boj2609.java
@@ -1,24 +1,32 @@
 package study.baekjoon.math;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj2609 {
-    public static void main(String[] args) {
-        // AxB = GCDxLCM
-        // LCM = AxB/GCD
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(gcd(a, b));
-        System.out.println((a*b)/gcd(a, b));
+    public static void main(String[] args) throws IOException {
+        //[백준] 최대공약수와 최대공배수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(GCD(a, b));
+        System.out.println((a*b)/GCD(a, b));
     }
 
-    public static int gcd(int x, int y) {
-        while (y != 0) {
-            int r = x%y;
-            x = y;
-            y = r;
+    /**
+     * 두 수 A,B의 최대공약수를 G, 최소공배수를 L 이라고 하면 다음 식이 성립한다
+     * AB = LG
+     * 유클리드 호제법: A를 B로 나눈 몫을 Q라 하고, 나머지를 R이라 하자. 이 때 GCD(A,B) = GCD(B,R)
+     */
+    public static int GCD(int a, int b) {
+        while (b != 0) {
+            int r = a%b;
+            a = b;
+            b = r;
         }
-        return x;
+        return a;
     }
 }
