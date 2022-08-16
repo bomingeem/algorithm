24464f8a1796b9d1c98bcc3b08113b1d39d7ed66
bomingeem <bomingeem@gmail.com>
2022-08-16 10:41:25 +0900

    [feat] 소수 찾기

--- src/study/baekjoon/math/boj1978.java
+++ src/study/baekjoon/math/boj1978.java
@@ -1,28 +1,32 @@
 package study.baekjoon.math;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj1978 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        int count = 0;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        //[백준] 소수 찾기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        while (size-- > 0) {
-            if (prime(scanner.nextInt())) {
+        while (st.hasMoreTokens()) {
+            if (isPrime(Integer.parseInt(st.nextToken()))) {
                 count++;
             }
         }
         System.out.println(count);
     }
 
-    public static boolean prime(int x) {
-        if (x < 2) {
-            return false;
-        }
-        for(int i=2; i<=x-1; i++) {
-            if(x % i == 0) {
-                return  false;
+    public static boolean isPrime(int x) {
+        if (x == 1) return false;
+
+        for (int i=2; i<x-1; i++) {
+            if (x % i == 0) {
+                return false;
             }
         }
         return true;
