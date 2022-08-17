fe6c9547b714ae722c076f25c37808773f721ad5
bomingeem <bomingeem@gmail.com>
2022-08-17 14:16:25 +0900

    [feat] 소수 구하기

--- src/study/baekjoon/math/boj1929.java
+++ src/study/baekjoon/math/boj1929.java
@@ -1,29 +1,40 @@
 package study.baekjoon.math;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj1929 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static int M;
+    static int N;
+    static boolean[] prime;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        //[백준] 소수 구하기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        prime = new boolean[N + 1];
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        boolean[] check = new boolean[b+1];
+         findPrimeNumber();
 
-        //true = 소수아님, false = 소수
-        check[0] = check[1] = true;
-
-        for(int i=2; i*i<=b; i++) {
-            if (check[i] == true) { //소수의 배수만 체크
-                continue;
-            }
-            for(int j=2*i; j<=b; j+=i) {
-                check[j] = true;
-            }
+        for (int i=M; i<=N; i++) {
+            if (!prime[i]) sb.append(i).append("\n");
         }
-        for (int i=a; i<=b; i++) {
-            if (check[i] == false) {
-                System.out.println(i);
+        System.out.println(sb);
+    }
+
+    /**
+     * k=2부터 √N 이하까지 반복하여 자연수들 중 k를 제외한 k의 배수들을 제외시킨다
+     * 소수가 아닌 경우 true, 소수인 경우 false
+     */
+    public static void findPrimeNumber() {
+        prime[0] = prime[1] = true;
+        for (int i=2; i<Math.sqrt(prime.length); i++) {
+            for (int j=i*i; j<prime.length; j+=i) {
+                prime[j] = true;
             }
         }
     }
