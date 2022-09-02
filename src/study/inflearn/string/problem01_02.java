f5e413915827deab426d46073f932ceef9b6d16c
bomingeem <bomingeem@gmail.com>
2022-09-02 16:47:28 +0900

    [fix] 대소문자 변환: 구현 방법을 solution 메서드로 구현

--- src/study/inflearn/string/problem01_02.java
+++ src/study/inflearn/string/problem01_02.java
@@ -7,17 +7,24 @@ import java.io.InputStreamReader;
 public class problem01_02 {
     public static void main(String[] args) throws IOException {
         //[인프런] 대소문자 변환
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //StuDY
         String word = br.readLine();
+        System.out.println(solution(word));
+    }
 
-        for (char ch : word.toCharArray()) {
-            if (Character.isUpperCase(ch)) {
-                sb.append(Character.toLowerCase(ch));
+    public static String solution(String str) {
+        /**
+         * 대문자 ASCII 넘버: 65~90
+         * 소문자 ASCII 넘버: 97~122
+         */
+        String answer = "";
+        for (char ch : str.toCharArray()) {
+            if (ch >= 97 && ch <= 122) {
+                answer += (char)(ch-32);
             } else {
-                sb.append(Character.toUpperCase(ch));
+                answer += (char)(ch+32);
             }
         }
-        System.out.println(sb);
+        return answer;
     }
 }
