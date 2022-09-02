bf963b8b8a641bae7290d6c0cb5dbd801d7444de
bomingeem <bomingeem@gmail.com>
2022-09-02 16:36:45 +0900

    [fix] 문자 찾기: 구현 방법을 solution 메서드로 구현

--- src/study/inflearn/string/problem01_01.java
+++ src/study/inflearn/string/problem01_01.java
@@ -8,12 +8,16 @@ public class problem01_01 {
     public static void main(String[] args) throws IOException {
         //[인프런] 문자 찾기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = 0;
         String word = br.readLine().toLowerCase();
         char character = br.readLine().toLowerCase().charAt(0);
-        for (char ch : word.toCharArray()) {
-            if (character == ch) count++;
+        System.out.println(solution(word, character));
+    }
+
+    public static int solution(String str, char t) {
+        int answer = 0;
+        for (char ch : str.toCharArray()) {
+            if (t == ch) answer++;
         }
-        System.out.println(count);
+        return answer;
     }
 }
