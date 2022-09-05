fdb111e2443f2b875a9613b9c9c71527b8f36e74
bomingeem <bomingeem@gmail.com>
2022-09-05 19:13:53 +0900

    [fix] 단어 뒤집기: 문제 풀이 방식 변경

--- src/study/inflearn/string/problem01_04.java
+++ src/study/inflearn/string/problem01_04.java
@@ -3,25 +3,52 @@ package study.inflearn.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 public class problem01_04 {
-    static Stack<Character> stack = new Stack<>();
     static int N;
     public static void main(String[] args) throws IOException {
         //[인프런] 단어 뒤집기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        String[] stringList = new String[N];
 
         for (int i=0; i<N; i++) {
-            String word = br.readLine();
-            for (char ch : word.toCharArray()) {
-                stack.push(ch);
-            }
-            while (!stack.isEmpty()) {
-                System.out.print(stack.pop());
-            }
-            System.out.println();
+            stringList[i] = br.readLine();
         }
+
+        for (String x : solution(stringList)) {
+            System.out.println(x);
+        }
+
+    }
+
+    public static List<String> solution(String[] str) {
+        List<String> answer = new ArrayList<>();
+        /**
+         * 2. 직접 뒤집는 방법
+         */
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length()-1;
+            while (lt < rt) {
+                char temp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = temp;
+                lt++;
+                rt--;
+            }
+            String result = String.valueOf(s);
+            answer.add(result);
+        }
+        /**
+         * 1. StringBuilder.reverse()를 이용한 풀이
+         */
+//        for (String x : str) {
+//            String temp = new StringBuilder(x).reverse().toString();
+//            answer.add(temp);
+//        }
+        return answer;
     }
 }
