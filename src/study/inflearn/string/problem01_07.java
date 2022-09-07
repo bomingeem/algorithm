eff2fc4ec201f9b7c373586102d28d328d39e8ab
bomingeem <bomingeem@gmail.com>
2022-09-07 13:41:14 +0900

    [fix] 회문 문자열: 직접 비교를 통한 풀이 추가

--- src/study/inflearn/string/problem01_07.java
+++ src/study/inflearn/string/problem01_07.java
@@ -13,14 +13,26 @@ public class problem01_07 {
     }
 
     public static String solution(String str) {
-        String answer = "";
+        String answer = "YES";
         str = str.toLowerCase();
-        String reverseStr = new StringBuilder(str).reverse().toString();
-        if (str.equals(reverseStr)) {
-            answer = "YES";
-        } else {
-            answer = "NO";
+
+        /**
+         * 1. 직접 비교를 통한 풀이
+         */
+        int length = str.length();
+        for (int i=0; i<length/2; i++) {
+            if (str.charAt(i) != str.charAt(length-i-1)) {
+                return "NO";
+            }
         }
+
+        /**
+         * 2. StringBuilder.reverse()를 통한 풀이
+         */
+//        String reverseStr = new StringBuilder(str).reverse().toString();
+//        if (!str.equals(reverseStr)) {
+//            answer = "NO";
+//        }
         return answer;
     }
 }
