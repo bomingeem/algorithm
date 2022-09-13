2b131daecd5cbbf2b0385601e04559f9d66efa93
bomingeem <bomingeem@gmail.com>
2022-09-13 11:01:13 +0900

    [feat] 암호

--- src/study/inflearn/string/problem01_12.java
+++ src/study/inflearn/string/problem01_12.java
@@ -15,7 +15,6 @@ public class problem01_12 {
 
     public static String solution(int n, String str) {
         String answer = "";
-        //TODO: 1. 이진수로 변경 2. 10진수화 3. 아스키 번호 65문자화
         for (int i=0; i<n; i++) {
             String temp = str.substring(0, 7).replace('#', '1').replace('*', '0');
             int number = Integer.parseInt(temp, 2);
