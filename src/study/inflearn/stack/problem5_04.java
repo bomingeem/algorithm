6c025df0d6c09b2a680f0e4eb41ab56d4c6cc491
bomingeem <bomingeem@gmail.com>
2022-11-03 22:10:53 +0900

    [feat] 후위식 연산

--- src/study/inflearn/stack/problem5_04.java
+++ src/study/inflearn/stack/problem5_04.java
@@ -1,40 +1,37 @@
 package study.inflearn.stack;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class problem5_04 {
-    public static void main(String[] args) {
-        //후위식 연산(postfix)
-        Solution solution = new Solution();
-        Scanner scanner = new Scanner(System.in);
-        String postfix = scanner.next();
-        System.out.println(solution.solution(postfix));
+    public static void main(String[] args) throws IOException {
+        //[인프런] 후위식 연산
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String postfix = br.readLine();
+        System.out.println(solution(postfix));
     }
 
-    static class Solution {
-        public int solution(String str) {
-            int answer = 0;
-            Stack<Integer> stack = new Stack<>();
-            for(char ch : str.toCharArray()) {
-                if(Character.isDigit(ch)) {
-                    stack.push(ch-48);
-                } else {
-                    int rt = stack.pop();
-                    int lt = stack.pop();
-                    if (ch == '+') {
-                        stack.push(lt+rt);
-                    } else if (ch == '-') {
-                        stack.push(lt-rt);
-                    } else if (ch == '*') {
-                        stack.push(lt*rt);
-                    } else if (ch == '/') {
-                        stack.push(lt/rt);
-                    }
+    public static int solution(String postfix) {
+        Stack<Integer> stack = new Stack<>();
+        for (char ch : postfix.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                stack.push(ch - 48);
+            } else {
+                Integer right = stack.pop();
+                Integer left = stack.pop();
+                if (ch == '+') {
+                    stack.push(left + right);
+                } else if (ch == '-') {
+                    stack.push(left - right);
+                } else if (ch == '*') {
+                    stack.push(left * right);
+                } else if (ch == '/') {
+                    stack.push(left / right);
                 }
             }
-            answer = stack.get(0);
-            return answer;
         }
+        return stack.get(0);
     }
 }
