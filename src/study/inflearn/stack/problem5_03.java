b1d3133c118545de7f23ea4957849d3e76f12ee3
bomingeem <bomingeem@gmail.com>
2022-11-03 21:46:26 +0900

    [feat] 크레인 인형뽑기

--- src/study/inflearn/stack/problem5_03.java
+++ src/study/inflearn/stack/problem5_03.java
@@ -1,50 +1,54 @@
 package study.inflearn.stack;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class problem5_03 {
-    public static void main(String[] args) {
-        //크레인 인형뽑기
-        Solution solution = new Solution();
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        //[인프런] 크레인 인형뽑기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] board = new int[N][N];
 
-        int n = scanner.nextInt();
-        int[][] board = new int[n][n];
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                board[i][j] = scanner.nextInt();
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j=0; j<N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int m = scanner.nextInt();
-        int[] moves = new int[m];
-        for (int i=0; i<m; i++) {
-            moves[i] = scanner.nextInt();
-        }
-        System.out.println(solution.solution(board, moves));
 
+        int M = Integer.parseInt(br.readLine());
+        int[] moves = new int[M];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i=0; i<M; i++) {
+            moves[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(board, moves));
     }
 
-    static class Solution {
-        public int solution(int[][] board, int[] moves) {
-            int answer = 0;
-            Stack<Integer> basket = new Stack<>();
-            basket.push(0);
-            for(int move : moves) {
-                for(int i=0; i<board.length; i++) { //board.length: 행의 크기
-                    if (board[i][move-1] != 0) { //0번째 index 부터 시작하므로 move-1
-                        if (basket.peek() == board[i][move-1]) {
-                            basket.pop();
-                            answer += 2;
-                        } else { //다른 인형일 경우
-                            basket.push(board[i][move-1]);
-                        }
-                        board[i][move-1] = 0; //꺼냈으면 0을 해야한다
-                        break; //더 꺼내면 안되니까 break 문을 건다
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> basket = new Stack<>();
+
+        for (int move : moves) {
+            for (int i=0; i<board.length; i++) {
+                if (board[i][move-1] != 0) {
+                    if (!basket.isEmpty() && basket.peek() == board[i][move-1]) {
+                        basket.pop();
+                        answer += 2;
+                    } else {
+                        basket.push(board[i][move-1]);
                     }
+                    board[i][move-1] = 0;
+                    break;
                 }
             }
-            return answer;
         }
+        return answer;
     }
 }
