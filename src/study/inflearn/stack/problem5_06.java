147ea124c5a78016ce5a3f7980733271c78c89a4
bomingeem <bomingeem@gmail.com>
2022-11-06 18:59:07 +0900

    [feat] 공주 구하기

--- src/study/inflearn/stack/problem5_06.java
+++ src/study/inflearn/stack/problem5_06.java
@@ -1,31 +1,36 @@
 package study.inflearn.stack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class problem5_06 {
-    public static void main(String[] args) {
-        //공주 구하기
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        problem5_06 problem = new problem5_06();
-        System.out.println(problem.solution(n, k));
+    public static void main(String[] args) throws IOException {
+        //[인프런] 공주 구하기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        System.out.println(solution(N, K));
     }
-    public int solution(int n, int k) {
+
+    static int solution(int n, int k) {
         int answer = 0;
         Queue<Integer> queue = new LinkedList<>();
+
         for (int i=1; i<=n; i++) {
             queue.offer(i);
         }
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             for (int i=1; i<k; i++) {
                 queue.offer(queue.poll());
             }
             queue.poll();
-            if(queue.size() == 1) {
+            if (queue.size() == 1) {
                 answer = queue.poll();
             }
         }
