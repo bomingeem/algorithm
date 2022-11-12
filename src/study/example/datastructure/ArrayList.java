8a3695de3fb490b82749ce7da84312e4c653790a
bomingeem <bomingeem@gmail.com>
2022-11-12 15:21:22 +0900

    fix(ArrayList): get, set, indexOf, contains, remove 메서드 구현

--- src/study/example/datastructure/ArrayList.java
+++ src/study/example/datastructure/ArrayList.java
@@ -48,32 +48,70 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public boolean remove(Object value) {
-        return false;
+        int index = indexOf(value);
+        if (index == -1) {
+            return false;
+        }
+        remove(index);
+        return true;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E remove(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        E element = (E) elementData[index];
+        elementData[index] = null;
+
+        for (int i=index; i<size-1; i++) {
+            elementData[i] = elementData[i+1];
+            elementData[i+1] = null;
+        }
+
+        size--;
+        resize();
+        return element;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E get(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (E) elementData[index];
     }
 
     @Override
     public void set(int index, E value) {
-
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            elementData[index] = value;
+        }
     }
 
     @Override
     public boolean contains(Object value) {
-        return false;
+        if (indexOf(value) >= 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public int indexOf(Object value) {
-        return 0;
+        int i;
+        for (i=0; i<size; i++) {
+            if (elementData[i].equals(value)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
@@ -89,6 +127,15 @@ public class ArrayList<E> implements List<E> {
     @Override
     public void clear() {
 
+    }
+
+    public int lastIndexOf(Object value) {
+        for (int i=size-1; i>=0; i--) {
+            if (elementData[i].equals(value)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public void addFirst(E value) {
