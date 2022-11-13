118bdc5c1136083fe8060cd8ce3091236a041dcf
bomingeem <bomingeem@gmail.com>
2022-11-13 14:03:55 +0900

    fix(Singly-LinkedList): get, set, indexOf, contains 메서드 구현

--- src/study/example/datastructure/SinglyLinkedList.java
+++ src/study/example/datastructure/SinglyLinkedList.java
@@ -175,22 +175,32 @@ public class SinglyLinkedList<E> implements List<E> {
 
     @Override
     public E get(int index) {
-        return null;
+        return search(index).data;
     }
 
     @Override
     public void set(int index, E value) {
-
+        Node<E> replaceNode = search(index);
+        replaceNode.data = null;
+        replaceNode.data = value;
     }
 
     @Override
     public boolean contains(Object value) {
-        return false;
+        return indexOf(value) >= 0;
     }
 
     @Override
     public int indexOf(Object value) {
-        return 0;
+        int index = 0;
+
+        for (Node<E> x = head; x != null; x = x.next) {
+            if (value.equals(x.data)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 
     @Override
