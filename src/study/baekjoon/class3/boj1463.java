958b0780049d85b256f8d35c779c90aa760e1160
bomingeem <bomingeem@gmail.com>
2022-12-18 11:28:06 +0900

    [feat] 1로 만들기

--- src/study/baekjoon/class3/boj1463.java
+++ src/study/baekjoon/class3/boj1463.java
@@ -1,53 +1,42 @@
 package study.baekjoon.class3;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class boj1463 {
-    static Integer[] dp;
-    public static void main(String[] args) {
-        //1로 만들기
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        dp = new Integer[N+1];
-        dp[0] = dp[1] = 0;
-        System.out.println(calculate(N));
+    static int[] d;
+    public static void main(String[] args) throws IOException {
+        //[백준] 1로 만들기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        d = new int[N+1];
+        System.out.println(solution(N));
     }
 
-    public static int calculate(int number) {
-        if (dp[number] == null) {
-            //2와 3의 배수인 6에 대한 케이스는 생각치도 못했다
-            if (number%6 == 0) {
-                dp[number] = Math.min(calculate(number-1), Math.min(calculate(number/3), calculate(number/2)))+1;
-            } else if (number%3 == 0) {
-                dp[number] = Math.min(calculate(number/3), calculate(number-1))+1;
-            } else if (number%2 == 0) {
-                dp[number] = Math.min(calculate(number/2), calculate(number-1))+1;
-            } else {
-                dp[number] = calculate(number-1)+1;
+    /**
+        D[i] = i를 1로 만드느데 필요한 최소 연산 횟수
+        i에게 가능한 경우를 생각해보자
+            1. i가 3으로 나누어 떨어졌을 때 3으로 나누는 경우 D[i/3] + 1
+            2. i가 2으로 나누어 떨어졌을 때 2으로 나누는 경우 D[i/2] + 1
+            3. i에서 1을 빼는 경우 D[i-1] + 1
+          → N/3 → ... → 1
+        N → ... → ... → 1
+          → N/2 → ... → 1
+     */
+    public static int solution(int n) {
+        //Bottom-up 방식
+        d[1] = 0;
+        for (int i=2; i<=n; i++) {
+            d[i] = d[i-1] + 1;
+            if (i%2 == 0 && d[i] > d[i/2] + 1) {
+                d[i] = d[i/2] + 1;
+            }
+            if (i%3 == 0 && d[i] > d[i/3] + 1) {
+                d[i] = d[i/3] + 1;
             }
         }
-        return dp[number];
+        return d[n];
     }
-
-    //바보같은 풀이
-    /*
-    public static void calculate(int number) {
-        if (number == 1) {
-            return;
-        }
-        if (number%3 == 0) {
-            int num = number/3;
-            answer++;
-            calculate(num);
-        } else if (number%2 == 0) {
-            int num = number/2;
-            answer++;
-            calculate(num);
-        } else {
-            int num = number-1;
-            answer++;
-            calculate(num);
-        }
-    }
-    */
 }
