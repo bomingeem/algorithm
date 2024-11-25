d220a5192a0dca59c5c3bf4a4591ea3def2aec0c
bomingeem <bomingeem@gmail.com>
2024-11-25 20:40:24 +0900

    fix: 기사단원의 무기

--- src/study/programmers/level0/prg136798.java
+++ src/study/programmers/level0/prg136798.java
@@ -2,29 +2,27 @@ package study.programmers.level0;
 
 public class prg136798 {
     public static void main(String[] args) {
-        //NOTE: 테스트 11~16,18,24,25 실패 (시간 초과)
+        // 1번째 방법 풀이 시 number = 10000 → 84 소요
+        // 2번째 방법 풀이 시 number = 10000 → 6 소요
         solution(10, 3, 2);
     }
 
     public static int solution(int number, int limit, int power) {
-        int[] knights = new int[number];
         int answer = 0;
         for (int i=0; i<number; i++) {
             int count = 0;
             int divisors = i+1;
-            for (int j=1; j<=divisors; j++) {
-                if (divisors % j == 0) {
+            for (int j=1; j*j<=divisors; j++) {
+                if (j*j == divisors) {
                     count++;
+                } else if (divisors % j == 0) {
+                    count += 2;
                 }
             }
-            knights[i] = count;
-        }
-        for (int knight : knights) {
-            if (knight > limit) {
-                answer += power;
-            } else {
-                answer += knight;
+            if (count > limit) {
+                count = power;
             }
+            answer += count;
         }
         return answer;
     }
